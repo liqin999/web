f37d2bd32314d132d0feae69e0ed78ebdc77f537
liqin <1483510623@qq.com>
2019-01-09 19:27:18 +0800

    yh

--- src/pages/dataService/dataService.vue
+++ src/pages/dataService/dataService.vue
@@ -4,13 +4,8 @@
         <el-aside width="250px">
             <div class="tabTree">
                 <el-tabs v-model="activeTreeName"
-<<<<<<< HEAD
-                            :stretch=true
-                            @tab-click="switchTreeMemu">
-=======
                          :stretch="true"
                          @tab-click="switchTreeMemu">
->>>>>>> origin/sww-dev
                     <el-tab-pane label="成品稿"
                                  name="first">
                         <left-menu :treeData="data2"
@@ -56,13 +51,8 @@
             <el-main ref="mainTable">
 
                 <el-tabs v-model="activeTreeName1"
-<<<<<<< HEAD
-                            :stretch=true
-                            @tab-click="switchTreeMemu1">
-=======
                          :stretch="true"
                          @tab-click="switchTreeMemu1">
->>>>>>> origin/sww-dev
                     <el-tab-pane label="全部(12888)"
                                  name="first1">
 
